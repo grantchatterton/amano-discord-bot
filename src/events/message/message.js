492b6182a441f58233fec9eccef02b84b3924eef
Grant Chatterton <grantchatterton@gmail.com>
2024-12-12 15:28:46 -0600

    Implement message reply functionality

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,6 +1,22 @@
-import { Events } from 'discord.js';
+import { AttachmentBuilder, Events } from 'discord.js';
+import * as config from './config.js';
+import { getRandomQuote } from './util.js';
 
 export default {
 	name: Events.MessageCreate,
-	async execute(message) {},
+	async execute(message) {
+		await message.channel.sendTyping();
+
+		setTimeout(async () => {
+			try {
+				await message.reply({
+					content: getRandomQuote(),
+					files: [new AttachmentBuilder(config.MESSAGE_IMAGE)],
+				});
+				console.log(`Replied to message ${message.content}`);
+			} catch (error) {
+				console.error(error);
+			}
+		}, config.MESSAGE_DELAY * 1_000);
+	},
 };
