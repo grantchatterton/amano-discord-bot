0ee91ffa7a4f2c945304fdba80077a5e5760bc31
Grant Chatterton <grantchatterton@gmail.com>
2024-12-12 15:22:56 -0600

    Move config constants to their own file

---
+++ src/events/message/config.js
@@ -0,0 +1,3 @@
+export const MESSAGE_CHANCE = 25; // Chance (percentage) of replying to a message
+export const MESSAGE_DELAY = 1; // Delay (in seconds) before replying
+export const MESSAGE_IMAGE = 'https://static.wikia.nocookie.net/aceattorney/images/4/4e/Ernest_Placating_1.gif'; // Image to send with the message
--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,9 +1,5 @@
 import { Events } from 'discord.js';
 
-const MESSAGE_CHANCE = 25; // Chance (percentage) of replying to a message
-const MESSAGE_DELAY = 1; // Delay (in seconds) before replying
-const MESSAGE_IMAGE = 'https://static.wikia.nocookie.net/aceattorney/images/4/4e/Ernest_Placating_1.gif'; // Image to send with the message
-
 export default {
 	name: Events.MessageCreate,
 	async execute(message) {},
