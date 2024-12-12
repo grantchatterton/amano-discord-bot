57ca82fd7addaad5bb0f1fd85de5b2b2b2009433
Grant Chatterton <grantchatterton@gmail.com>
2024-12-12 15:30:41 -0600

    Implement message reply functionality

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,22 +1,28 @@
 import { AttachmentBuilder, Events } from 'discord.js';
 import * as config from './config.js';
-import { getRandomQuote } from './util.js';
+import * as util from './util.js';
 
 export default {
 	name: Events.MessageCreate,
 	async execute(message) {
-		await message.channel.sendTyping();
+		if (
+			!message.author.bot && // make sure a bot didn't send the message (including the one here)
+			util.hasSwear(message.content) && // ensure the message contains a swear
+			util.shouldSendMessage() // determine if we should send a reply (based on "MESSAGE_CHANCE" config variable)
+		) {
+			await message.channel.sendTyping();
 
-		setTimeout(async () => {
-			try {
-				await message.reply({
-					content: getRandomQuote(),
-					files: [new AttachmentBuilder(config.MESSAGE_IMAGE)],
-				});
-				console.log(`Replied to message ${message.content}`);
-			} catch (error) {
-				console.error(error);
-			}
-		}, config.MESSAGE_DELAY * 1_000);
+			setTimeout(async () => {
+				try {
+					await message.reply({
+						content: util.getRandomQuote(),
+						files: [new AttachmentBuilder(config.MESSAGE_IMAGE)],
+					});
+					console.log(`Replied to message ${message.content}`);
+				} catch (error) {
+					console.error(error);
+				}
+			}, config.MESSAGE_DELAY * 1_000);
+		}
 	},
 };
