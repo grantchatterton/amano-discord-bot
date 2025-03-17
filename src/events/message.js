e585941baf0f4f8e238de318877422b23d689d33
Grant Chatterton <grantchatterton@gmail.com>
2025-03-17 14:21:30 -0500

    Add support for replying to messages with "i"

--- src/events/message.js
+++ src/events/message.js
@@ -5,16 +5,21 @@ import { getRandomQuote, hasSwear, shouldReplyToMessage } from '../util/util.js'
 export default {
 	name: Events.MessageCreate,
 	async execute(message) {
-		if (
-			!message.author.bot && // Make sure a bot didn't send the message (including this one)
-			hasSwear(message.content) && // Make sure the message contains a swear
-			shouldReplyToMessage() // Make sure we should actually reply to the message (based on the chance of it happening)
-		) {
-			// Reply to the message!
-			await message.reply({
-				content: getRandomQuote(),
-				files: [new AttachmentBuilder(MESSAGE_SWEAR_REPLY_IMAGE)],
-			});
+		// Make sure a bot didn't send the message (including this one)
+		if (!message.author.bot) {
+			// Check if the message consists of just "i"
+			if (message.content === 'i') {
+				// Reply to the message!
+				await message.reply('e');
+			}
+			// Check if the message contains a swear and whether we should reply to it
+			else if (hasSwear(message.content) && shouldReplyToMessage()) {
+				// Reply to the message!
+				await message.reply({
+					content: getRandomQuote(),
+					files: [new AttachmentBuilder(MESSAGE_SWEAR_REPLY_IMAGE)],
+				});
+			}
 		}
 	},
 };
