f4f5f780a2e044468e8da1683ef3b4f391fd5b2b
Grant Chatterton <grantchatterton@gmail.com>
2025-03-31 13:42:37 -0500

    Call initReplies() earlier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import process from 'node:process';
 import { URL } from 'node:url';
 import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import initReplies from './replies.js';
 import { loadCommands, loadEvents } from './util/loaders.js';
 import { registerEvents } from './util/registerEvents.js';
+
+// Initialize the replies
+initReplies();
 
 // Initialize the client
 const client = new Client({
