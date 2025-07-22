50f25446cd44413da6904f9dbba0f92287a61bf3
Grant Chatterton <grantchatterton@gmail.com>
2025-07-22 18:06:18 -0500

    Update replies.js

--- src/replies.js
+++ src/replies.js
@@ -4,5 +4,5 @@ export default () => {
 	addReply('i', 'e');
 	addReply('there', 'fore');
 	addReply('hey ernest', 'https://i.imgflip.com/9n5ftr.gif');
-    addReply('hey ernest, what do you think of black sabbath?', 'https://tenor.com/view/no-family-guy-bad-dog-newspaper-peter-gif-8108196576766934699');
+  addReply('hey ernest, what do you think of black sabbath?', 'https://tenor.com/view/no-family-guy-bad-dog-newspaper-peter-gif-8108196576766934699');
 };
