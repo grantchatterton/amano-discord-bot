c281f5b29d85cb535a5b12d12f59668b6ee6f8cc
Grant Chatterton <grantchatterton@gmail.com>
2025-09-18 16:05:52 -0500

    Update to include error handling

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,15 +1,19 @@
-import { AttachmentBuilder, Events } from 'discord.js';
-import { getRandomQuote, getReplyImage, hasSwear, shouldReplyToMessage } from '../util/util.js';
+import { AttachmentBuilder, Events } from "discord.js";
+import { getRandomQuote, getReplyImage, hasSwear, shouldReplyToMessage } from "../util/util.js";
 
 export default {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (!message.author.bot && hasSwear(message.content) && shouldReplyToMessage()) {
-			const [quote, image] = await Promise.all([getRandomQuote(), getReplyImage()]);
-			await message.reply({
-				content: quote,
-				files: [new AttachmentBuilder(image)],
-			});
+			try {
+				const [quote, image] = await Promise.all([getRandomQuote(), getReplyImage()]);
+				await message.reply({
+					content: quote,
+					files: [new AttachmentBuilder(image)],
+				});
+			} catch (error) {
+				console.error(error);
+			}
 		}
 	},
 };
