2b267c81863c5bc5ab13ddba6481208503aae3df
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 12:08:11 -0500

    Add TODO

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,6 +6,7 @@ export default {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
+		// TODO: Modify to make safer for production
 		await sequelize.sync({ alter: true });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
