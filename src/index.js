d4a2e5294cb0c8a7561c796badc3a573e518550b
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 10:37:05 -0500

    Add conditional loading of .env file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import 'dotenv/config';
-import process from 'node:process';
-import { URL } from 'node:url';
-import { Client, GatewayIntentBits, Partials } from 'discord.js';
-import { loadCommands, loadEvents } from './util/loaders.js';
-import { registerEvents } from './util/registerEvents.js';
+import path from "node:path";
+import process from "node:process";
+import { URL } from "node:url";
+import appRootPath from "app-root-path";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import dotenv from "dotenv";
+import { loadCommands, loadEvents } from "./util/loaders.js";
+import { registerEvents } from "./util/registerEvents.js";
+
+// Initialize environment variables
+dotenv.config({ path: path.join(appRootPath.path, `.env.${process.env.NODE_ENV || "development"}`) });
 
 // Initialize the client
 const client = new Client({
@@ -17,8 +22,8 @@ const client = new Client({
 });
 
 // Load the events and commands
-const events = await loadEvents(new URL('events/', import.meta.url));
-const commands = await loadCommands(new URL('commands/', import.meta.url));
+const events = await loadEvents(new URL("events/", import.meta.url));
+const commands = await loadCommands(new URL("commands/", import.meta.url));
 
 // Register the event handlers
 registerEvents(commands, events, client);
@@ -37,5 +42,5 @@ async function shutdown() {
 	}
 }
 
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
