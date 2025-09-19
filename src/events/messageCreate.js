58145647661b0b5cfc48a474b6a17957249cfbeb
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 12:01:47 -0500

    Modify logic to retrieve and reply to a message

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,19 +1,16 @@
-import { AttachmentBuilder, Events } from "discord.js";
-import { getRandomQuote, getReplyImage, hasSwear, shouldReplyToMessage } from "../util/util.js";
+import { Events } from "discord.js";
+import { getMessageReply } from "../util/util.js";
 
 export default {
 	name: Events.MessageCreate,
 	async execute(message) {
-		if (!message.author.bot && hasSwear(message.content) && shouldReplyToMessage()) {
-			try {
-				const [quote, image] = await Promise.all([getRandomQuote(), getReplyImage()]);
-				await message.reply({
-					content: quote,
-					files: [new AttachmentBuilder(image)],
-				});
-			} catch (error) {
-				console.error(error);
+		try {
+			const reply = await getMessageReply(message);
+			if (reply) {
+				await message.reply(reply);
 			}
+		} catch (error) {
+			console.error(error);
 		}
 	},
 };
