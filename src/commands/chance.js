23db7d9c25c0183f98b6bc3625edf6c8780a0de6
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 12:18:45 -0500

    Add functionality for updating reply chance

---
+++ src/commands/chance.js
@@ -0,0 +1,38 @@
+import { ApplicationCommandOptionType, bold, channelLink, ChannelType } from "discord.js";
+import { channelService } from "../services/channelService.js";
+
+/** @type {import('./index.js').Command} */
+export default {
+	data: {
+		name: "chance",
+		description: "Set the chance of replying to a message containing a swear for a channel.",
+		options: [
+			{
+				name: "channel",
+				description: "Text channel to set reply chance for.",
+				type: ApplicationCommandOptionType.Channel,
+				channel_types: ChannelType.GuildText,
+				required: true,
+			},
+			{
+				name: "value",
+				description: "Value to set for the channel's reply chance.",
+				type: ApplicationCommandOptionType.Integer,
+				required: true,
+				min_value: 0,
+				max_value: 100,
+			},
+		],
+	},
+	async execute(interaction) {
+		const channel = interaction.options.getChannel("channel");
+		const chance = interaction.options.getInteger("value");
+		try {
+			await channelService.setChannelReplyChance(channel.id, chance);
+			await interaction.reply({ content: `Reply chance for ${channelLink(channel.id)} set to ${bold(chance)}` });
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: "Failure updating reply chance!", ephemeral: true });
+		}
+	},
+};
