a4e71892011c4cb8e0f74a22b6ec6e6e142ed3ed
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 11:47:18 -0500

    Misc changes

--- src/services/channelService.js
+++ src/services/channelService.js
@@ -1,9 +1,19 @@
 import { Collection } from "discord.js";
+import { MESSAGE_REPLY_CHANCE } from "../config.js";
 import Channel from "../models/channel.js";
 
 const channelCollection = new Collection();
 
 export const channelService = {
+	async addChannel(channelId) {
+		if (channelCollection.has(channelId)) {
+			return channelCollection.get(channelId);
+		}
+
+		const [channel] = await Channel.findOrCreate({ where: { channelId } });
+		channelCollection.set(channelId, channel);
+		return channel;
+	},
 	async getChannel(channelId) {
 		if (channelCollection.has(channelId)) {
 			return channelCollection.get(channelId);
@@ -16,13 +26,14 @@ export const channelService = {
 
 		return channel;
 	},
-	async addChannel(channelId) {
-		if (channelCollection.has(channelId)) {
-			return channelCollection.get(channelId);
-		}
-
-		const [channel] = await Channel.findOrCreate({ where: { channelId } });
-		channelCollection.set(channelId, channel);
+	async getChannelReplyChance(channelId) {
+		const channel = await this.getChannel(channelId);
+		return channel ? channel.replyChance : MESSAGE_REPLY_CHANCE;
+	},
+	async setChannelReplyChance(channelId, replyChance) {
+		const channel = await this.addChannel(channelId);
+		channel.replyChance = replyChance;
+		await channel.save();
 		return channel;
 	},
 };
