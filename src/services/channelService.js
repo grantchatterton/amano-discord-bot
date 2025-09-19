e3d371fffe2f2290278c830d3857dc425516449d
Grant Chatterton <grantchatterton@gmail.com>
2025-09-19 15:26:06 -0500

    Update to reduce number of DB queries

--- src/services/channelService.js
+++ src/services/channelService.js
@@ -1,31 +1,33 @@
 import { Collection } from "discord.js";
-import { MESSAGE_REPLY_CHANCE } from "../config.js";
 import Channel from "../models/channel.js";
 
 const channelCollection = new Collection();
 
 export const channelService = {
-	async addChannel(channelId) {
+	async getChannel(channelId) {
 		if (channelCollection.has(channelId)) {
 			return channelCollection.get(channelId);
 		}
 
-		const [channel] = await Channel.findOrCreate({ where: { channelId } });
-		channelCollection.set(channelId, channel);
-		return channel;
-	},
-	async getChannel(channelId) {
-		return this.addChannel(channelId);
+		const channel = await Channel.findOne({ where: { channelId } });
+		if (channel) {
+			channelCollection.set(channelId, channel);
+			return channel;
+		}
+
+		// Do this so we don't have to store in DB until we manually save it
+		const newChannel = Channel.build({ channelId });
+		channelCollection.set(channelId, newChannel);
+		return newChannel;
 	},
 	async getChannelReplyChance(channelId) {
 		const channel = await this.getChannel(channelId);
-		return channel ? channel.replyChance : MESSAGE_REPLY_CHANCE;
+		return channel.replyChance;
 	},
 	async setChannelReplyChance(channelId, replyChance) {
-		const channel = await this.addChannel(channelId);
+		const channel = await this.getChannel(channelId);
 		channel.replyChance = replyChance;
 		await channel.save();
-		return channel;
 	},
 };
 
