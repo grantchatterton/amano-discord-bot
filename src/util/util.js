79ee89ce76da7cd60c0d54a9565c778feeccb198
Grant Chatterton <grantchatterton@gmail.com>
2025-09-20 22:02:20 -0500

    Add OpenAI support

--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,7 @@
+import process from "node:process";
 import { AttachmentBuilder } from "discord.js";
-import { MESSAGE_REPLY_IMAGE } from "../config.js";
+import OpenAI from "openai";
+import AmanoImages from "../images.js";
 import { AMANO_QUOTES } from "../quotes.js";
 import { channelService } from "../services/channelService.js";
 import { SWEAR_PATTERNS } from "../swears.js";
@@ -46,12 +48,21 @@ export function hasSwear(message) {
 }
 
 /**
+ * Returns a random array element.
+ *
+ * @returns {any} Random element.
+ */
+export function getRandomElement(array) {
+	return array[getRandomInt(0, array.length - 1)];
+}
+
+/**
  * Returns a Promise containing a reply quote.
  *
  * @returns {Promise<string>} Random quote.
  */
 export async function getReplyQuote() {
-	return AMANO_QUOTES[getRandomInt(0, AMANO_QUOTES.length - 1)];
+	return getRandomElement(AMANO_QUOTES);
 }
 
 /**
@@ -60,7 +71,54 @@ export async function getReplyQuote() {
  * @returns {Promise<string>} Image URL.
  */
 export async function getReplyImage() {
-	return MESSAGE_REPLY_IMAGE;
+	return getRandomElement(AmanoImages.AMANO_PLACATING);
+}
+
+/**
+ * Returns a Promise containing an AI generated reply.
+ *
+ * @returns {Promise<string>} Reply content.
+ */
+const openAI = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+export async function getAIReply(message) {
+	// Determine a random mood
+	const moods = ["normal", "placating", "sad", "angry", "sweating"];
+	const randomMood = getRandomElement(moods);
+	let image;
+	switch (randomMood) {
+		case "normal":
+			image = getRandomElement(AmanoImages.AMANO_NORMAL);
+			break;
+		case "placating":
+			image = getRandomElement(AmanoImages.AMANO_PLACATING);
+			break;
+		case "sad":
+			image = getRandomElement(AmanoImages.AMANO_SAD);
+			break;
+		case "angry":
+			image = getRandomElement(AmanoImages.AMANO_ANGRY);
+			break;
+		case "sweating":
+			image = getRandomElement(AmanoImages.AMANO_SWEATING);
+			break;
+		default:
+			image = AmanoImages.AMANO_NORMAL[0];
+			break;
+	}
+
+	const MAX_MESSAGE_LENGTH = 2_000;
+	try {
+		const response = await openAI.responses.create({
+			model: "gpt-5-nano",
+			reasoning: { effort: "medium" },
+			instructions: `Talk like Ernest Amano from Ace Attorney. Start the response with 'Now, now', and talk in a way that someone who is ${randomMood} would. Keep the length to a maximum of ${MAX_MESSAGE_LENGTH}.`,
+			input: message,
+		});
+		return { content: response.output_text, files: [new AttachmentBuilder(image)] };
+	} catch (error) {
+		console.error(error);
+		return { content: "Now, now, I'm a little tired. Please try again later!" };
+	}
 }
 
 /**
@@ -75,8 +133,16 @@ export async function getMessageReply(message) {
 		return false;
 	}
 
+	const content = message.content;
+
+	// Handle case where the message starts with "hey ernest"
+	if (content.toLowerCase().startsWith("hey ernest")) {
+		const [reply] = await Promise.all([getAIReply(content), message.channel.sendTyping()]);
+		return reply;
+	}
+
 	// Check if the message contains a swear (stop if it doesn't)
-	if (!hasSwear(message.content)) {
+	if (!hasSwear(content)) {
 		return false;
 	}
 
@@ -89,16 +155,7 @@ export async function getMessageReply(message) {
 
 	// Return an object representing a message reply
 	const [quote, image] = await Promise.all([getReplyQuote(), getReplyImage()]);
-	return {
-		content: quote,
-		files: [new AttachmentBuilder(image)],
-	};
+	return { content: quote, files: [new AttachmentBuilder(image)] };
 }
 
-export default {
-	getRandomInt,
-	hasSwear,
-	getReplyQuote,
-	getReplyImage,
-	getMessageReply,
-};
+export default { getRandomInt, hasSwear, getRandomElement, getReplyQuote, getReplyImage, getAIReply, getMessageReply };
