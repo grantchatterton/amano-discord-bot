ac65ff1ad1b2ce202ad55a2fbd464769c5c42557
Grant Chatterton <grantchatterton@gmail.com>
2025-09-20 21:35:44 -0500

    Add constants for arrays of different GIFs

---
+++ src/images.js
@@ -0,0 +1,10 @@
+export const AMANO_NORMAL = ["https://static.wikia.nocookie.net/aceattorney/images/a/a6/Ernest_Normal_2.gif"];
+export const AMANO_PLACATING = [
+	"https://static.wikia.nocookie.net/aceattorney/images/4/4e/Ernest_Placating_1.gif",
+	"https://static.wikia.nocookie.net/aceattorney/images/9/92/Ernest_Placating_2.gif",
+];
+export const AMANO_SAD = ["https://static.wikia.nocookie.net/aceattorney/images/4/45/Ernest_Sad_2.gif"];
+export const AMANO_ANGRY = ["https://static.wikia.nocookie.net/aceattorney/images/c/cc/Ernest_Angry_2.gif"];
+export const AMANO_SWEATING = ["https://static.wikia.nocookie.net/aceattorney/images/5/58/Ernest_Sweating_2.gif"];
+
+export default { AMANO_NORMAL, AMANO_PLACATING, AMANO_SAD, AMANO_ANGRY, AMANO_SWEATING };
