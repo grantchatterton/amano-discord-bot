5828b353fa1e770a0ca97826a5b8fda23a090571
Grant Chatterton <grantchatterton@gmail.com>
2025-09-22 13:03:42 -0500

    Update to only import files ending in .js

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ export async function loadModels() {
 	try {
 		const files = await fs.readdir(import.meta.dirname);
 		for (const file of files) {
-			if (file !== "index.js") {
+			if (file !== "index.js" && file.endsWith(".js")) {
 				const module = await import(`./${file}`);
 				module.default(sequelize, DataTypes);
 			}
