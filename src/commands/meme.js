94dbe595d944c6a416e790beeb9931367cbc1f05
Grant Chatterton <grantchatterton@gmail.com>
2025-09-25 10:56:25 -0500

    Add "meme" command

---
+++ src/commands/meme.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+/** @type {import('./index.js').Command} */
+export default {
+	data: {
+		name: "meme",
+		description: "Fetch a random meme!",
+	},
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		try {
+			const response = await axios.get("https://meme-api.com/gimme");
+			return await interaction.editReply(response.data.url);
+		} catch (error) {
+			console.error(error);
+			return interaction.editReply("Something went wrong while fetching a meme.");
+		}
+	},
+};
