fe7bc312631fec524363cc221af4045f7e22deae
Grant Chatterton <grantchatterton@gmail.com>
2025-09-28 20:23:11 -0500

    Update meme.js

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,16 +1,27 @@
 import axios from "axios";
+import { ApplicationCommandOptionType } from "discord.js";
 
 /** @type {import('./index.js').Command} */
 export default {
 	data: {
 		name: "meme",
 		description: "Send a random meme!",
+		options: [
+			{
+				type: ApplicationCommandOptionType.String,
+				name: "subreddit",
+				description: "Subreddit to fetch the meme from.",
+				required: false,
+			},
+		],
 	},
 	async execute(interaction) {
+		const subreddit = interaction.options.getString("subreddit") ?? "";
+
 		await interaction.deferReply();
 
 		try {
-			const response = await axios.get("https://meme-api.com/gimme");
+			const response = await axios.get(`https://meme-api.com/gimme/${subreddit}`);
 			return await interaction.editReply(response.data.url);
 		} catch (error) {
 			console.error(error);
