3ad37462db77dc98dbd1381771a3ff20e9123013
Grant Chatterton <grantchatterton@gmail.com>
2025-10-10 21:24:26 -0500

    Update rich.js

--- src/commands/rich.js
+++ src/commands/rich.js
@@ -5,6 +5,6 @@ export default {
 		description: "Send a picture of ReviewTechUSA!",
 	},
 	async execute(interaction) {
-		await interaction.reply("https://exploreprofile.com/wp-content/uploads/2023/08/reviewtechusa.jpg");
+		await interaction.reply("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdOObsk6LpuM2Qlxn9w0CM-0uJdc7PJYaaaW1CabNAFYLqI0-Ahbb3qO4&s=10");
 	},
 };
