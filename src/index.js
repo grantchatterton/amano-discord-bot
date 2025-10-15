ea306adfef8bfc4dc51607c7d97cf5e1a8ae2f65
Grant Chatterton <grantchatterton@gmail.com>
2025-10-15 10:51:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./config.js";
+
 import process from "node:process";
 import { URL } from "node:url";
-import { bold, Client, GatewayIntentBits, Partials, WebhookClient, codeBlock } from "discord.js";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
 import { dbShutdown } from "./db/db.js";
 import { initDB } from "./db/dbInit.js";
 import { loadCommands, loadEvents } from "./util/loaders.js";
@@ -30,57 +31,6 @@ registerEvents(commands, events, client);
 
 // Login to the client
 void client.login(process.env.DISCORD_TOKEN);
-
-// Implement a custom error handler if an error webhook URL is loaded
-const { ERROR_WEBHOOK_URL } = process.env;
-if (ERROR_WEBHOOK_URL) {
-	const oldConsoleError = console.error;
-	console.error = async (...args) => {
-		// Log to the original console.error
-		oldConsoleError.apply(this, args);
-
-		// Format the error message for Discord
-		const errorMessage = args
-			.map((arg) => {
-				if (typeof arg === "object" && arg !== null) {
-					return JSON.stringify(arg);
-				}
-
-				return String(arg);
-			})
-			.join(" ");
-
-		// Attempt to send it to the webhook
-		try {
-			const webhookClient = new WebhookClient({ url: ERROR_WEBHOOK_URL });
-			await webhookClient.send({
-				content: `${bold("Error Detected")}\n${codeBlock(errorMessage)}`,
-				username: process.env.ERROR_WEBHOOK_USERNAME || "Ernest Amano (Error Reporter)",
-				avatarURL: process.env.ERROR_WEBHOOK_AVATAR || "",
-			});
-		} catch (error) {
-			// Log to the original console.error
-			oldConsoleError.apply(this, [error]);
-		}
-	};
-
-	// Define custom handler for uncaught exceptions
-	process.on("uncaughtException", (error) => {
-		console.error(error);
-		process.exit(1);
-	});
-
-	// Define custom handler for uncaught promise rejections
-	process.on("unhandledRejection", (error) => {
-		console.error(error);
-		process.exit(1);
-	});
-}
-
-if (process.env.NODE_ENV === "development") {
-	// Test the custom error handler
-	console.error("Test Message");
-}
 
 // Terminate peacefully when "SIGINT" or "SIGTERM" received
 async function shutdown() {
