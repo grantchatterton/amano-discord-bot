23f900e2aa3e61adf07680397457336691b1acde
Grant Chatterton <grantchatterton@gmail.com>
2025-10-19 12:05:28 -0500

    Add /roll command

---
+++ src/commands/roll.js
@@ -0,0 +1,41 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { getRandomInt } from "../util/util.js";
+
+/** @type {import('./index.js').Command} */
+export default {
+	data: {
+		name: "roll",
+		description: "Roll one or more dice",
+		options: [
+			{
+				type: ApplicationCommandOptionType.Integer,
+				name: "count",
+				description: "Number of dice to roll",
+				required: false,
+				min_value: 1,
+				max_value: 6,
+			},
+			{
+				type: ApplicationCommandOptionType.Integer,
+				name: "sides",
+				description: "Number of sides on each dice",
+				required: false,
+				min_value: 2,
+				max_value: 10,
+			},
+		],
+	},
+	async execute(interaction) {
+		const count = interaction.options.getInteger("count") ?? 1;
+		const sides = interaction.options.getInteger("sides") ?? 6;
+
+		if (count === 1) {
+			await interaction.reply(`You rolled: ${getRandomInt(1, sides)}`);
+		} else {
+			const response = Array.from({ length: count }, () => getRandomInt(1, sides))
+				.map((value, index) => `Dice #${index + 1}: ${value}`)
+				.join("\n");
+			await interaction.reply(response);
+		}
+	},
+};
