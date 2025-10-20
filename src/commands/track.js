6a4538df9dc392a4a3b68adeee292ad5525e680f
Grant Chatterton <grantchatterton@gmail.com>
2025-10-20 10:46:13 -0500

    Update track.js

--- src/commands/track.js
+++ src/commands/track.js
@@ -8,37 +8,29 @@ export default {
 		description: "Allows users to opt in/out of having their messages tracked",
 		options: [
 			{
-				type: ApplicationCommandOptionType.Subcommand,
-				name: "on",
-				description: "Opt-in to message tracking.",
-			},
-			{
-				type: ApplicationCommandOptionType.Subcommand,
-				name: "off",
-				description: "Opt-out of message tracking.",
+				type: ApplicationCommandOptionType.Boolean,
+				name: "status",
+				description: "Whether you wish to have messages tracked or not",
+				required: false,
 			},
 		],
 	},
 	async execute(interaction) {
-		const sub = interaction.options.getSubcommand();
+		const status = interaction.options.getBoolean("status");
 
 		await interaction.deferReply({ ephemeral: true });
 
 		try {
-			const user = await userService.getUser(interaction.user.id);
-			const status = sub === "on";
-			const subUpper = sub.toUpperCase();
-			if (user.trackMessages === status) {
-				return interaction.editReply(`Message tracking is already ${bold(subUpper)}.`);
+			const user = await userService.getUser(interaction.member.id);
+			if (status !== null && user.trackMessages !== status) {
+				user.trackMessages = status;
+				await user.save();
 			}
 
-			user.trackMessages = status;
-			await user.save();
-
-			return await interaction.editReply(`Message tracking ${bold(subUpper)}.`);
+			await interaction.editReply({ content: `Message tracking ${bold(user.trackMessages ? "ON" : "OFF")}` });
 		} catch (error) {
 			console.error(error);
-			return interaction.editReply("Something went wrong while trying to update your message tracking status.");
+			await interaction.editReply({ content: `An error occurred. Please try again later!` });
 		}
 	},
 };
