eda2f778c57d420ce48244be38c5d4c75644ef93
Grant Chatterton <grantchatterton@gmail.com>
2025-10-27 16:22:27 -0500

    feat: change model retrieval to use sequelize.models

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,18 +1,18 @@
 import { Collection } from "discord.js";
 import { sequelize } from "../db/db.js";
 
-const User = sequelize.model("User");
+const User = sequelize.models.User;
 
 const userCollection = new Collection();
 
 export const userService = {
-    async getUser(userId) {
-        if (!userCollection.has(userId)) {
-            const [user] = await User.findOrCreate({ where: { userId }, defaults: { userId } });
-            userCollection.set(userId, user);
-            return user;
-        }
+	async getUser(userId) {
+		if (!userCollection.has(userId)) {
+			const [user] = await User.findOrCreate({ where: { userId }, defaults: { userId } });
+			userCollection.set(userId, user);
+			return user;
+		}
 
-        return userCollection.get(userId);
-    },
+		return userCollection.get(userId);
+	},
 };
