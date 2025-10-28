58af40689983ffe06fdf58665191a9aa37ea23e5
Grant Chatterton <grantchatterton@gmail.com>
2025-10-28 07:30:19 -0500

    feat(deploy): remove db initialization from deploy script

--- src/util/deploy.js
+++ src/util/deploy.js
@@ -3,11 +3,7 @@ import process from "node:process";
 import { URL } from "node:url";
 import { API } from "@discordjs/core/http-only";
 import { REST } from "discord.js";
-import { initDB } from "../db/dbInit.js";
 import { loadCommands } from "./loaders.js";
-
-// Initialize the DB
-await initDB();
 
 const commands = await loadCommands(new URL("../commands/", import.meta.url));
 const commandData = [...commands.values()].map((command) => command.data);
