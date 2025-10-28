6a37438fad4543f4442c0a2640d038978f116a6f
Grant Chatterton <grantchatterton@gmail.com>
2025-10-28 08:04:12 -0500

    feat(config): add MAX_MESSAGE_LIMIT config option

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ dotenv.config({ path: path.join(appRootPath.path, `.env.${process.env.NODE_ENV |
 export const MESSAGE_REPLY_CHANCE = 25;
 export const MESSAGE_REPLY_IMAGE = "https://static.wikia.nocookie.net/aceattorney/images/4/4e/Ernest_Placating_1.gif";
 
-export default {
-	MESSAGE_REPLY_CHANCE,
-	MESSAGE_REPLY_IMAGE,
-};
+export const MAX_MESSAGE_LIMIT = (() => {
+	const limit = Number.parseInt(process.env.MAX_MESSAGE_LIMIT, 10);
+	return Number.isNaN(limit) ? 10 : limit;
+})();
