1239b8b999b01e1d60d9b638da7fac64283175a8
Grant Chatterton <grantchatterton@gmail.com>
2025-10-27 20:45:57 -0500

    feat: Create ernest.js

---
+++ src/commands/ernest.js
@@ -0,0 +1,30 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { getAIReply } from "../util/util.js";
+
+/** @type {import('./index.js').Command} */
+export default {
+	data: {
+		name: "ernest",
+		description: "Send a message to Ernest Amano",
+		options: [
+			{
+				type: ApplicationCommandOptionType.String,
+				name: "message",
+				description: "Message to send",
+				required: true,
+			},
+		],
+	},
+	async execute(interaction) {
+		const message = interaction.options.getString("message");
+
+		await interaction.deferReply();
+
+		const response = await getAIReply({
+            author: interaction.member,
+            content: message,
+            guildId: interaction.guildId,
+        });
+        await interaction.editReply(response);
+	},
+};
